888ed72c6da823e7bddcbc40975fb9e680a4bd4c
Daniel Chalef <131175+danielchalef@users.noreply.github.com>
2023-08-26 12:26:45 -0700

    user class docs

--- src/user_manager.ts
+++ src/user_manager.ts
@@ -2,7 +2,13 @@ import { User, CreateUserRequest, UpdateUserRequest } from "./user_models";
 import { Session } from "./memory_models";
 import { IZepClient } from "./interfaces";
 import { API_BASEURL, handleRequest } from "./utils";
+import { APIError, NotFoundError } from "./errors";
 
+/**
+ * UserManager class handles all user related operations such as
+ * adding, getting, updating, deleting, listing users and their sessions.
+ * It uses the IZepClient interface to make requests to the server.
+ */
 export default class UserManager {
    client: IZepClient;
 
@@ -14,6 +20,13 @@ export default class UserManager {
       return `${this.client.baseURL}${API_BASEURL}${endpoint}`;
    }
 
+   /**
+    * Add a new user.
+    *
+    * @param {CreateUserRequest} user - The user details to be added.
+    * @returns {Promise<User>} A Promise that resolves to a User object.
+    * @throws {APIError} If the request fails.
+    */
    async add(user: CreateUserRequest): Promise<User> {
       const response = await handleRequest(
          fetch(this.getFullUrl(`/user`), {
@@ -32,6 +45,14 @@ export default class UserManager {
       return new User(responseData);
    }
 
+   /**
+    * Get a user by their ID.
+    *
+    * @param {string} userId - The ID of the user to be retrieved.
+    * @returns {Promise<User>} A Promise that resolves to a User object.
+    * @throws {NotFoundError} If the request no user is found for the given ID.
+    * @throws {APIError} If the request fails.
+    */
    async get(userId: string): Promise<User> {
       const response = await handleRequest(
          fetch(this.getFullUrl(`/user/${userId}`), {
@@ -45,6 +66,14 @@ export default class UserManager {
       return new User(responseData);
    }
 
+   /**
+    * Update a user's details.
+    *
+    * @param {UpdateUserRequest} user - The updated user details.
+    * @returns {Promise<User>} A Promise that resolves to a User object with the updated details.
+    * @throws {NotFoundError} If the request no user is found for the given ID.
+    * @throws {APIError} If the request fails.
+    */
    async update(user: UpdateUserRequest): Promise<User> {
       const response = await handleRequest(
          fetch(this.getFullUrl(`/user/${user.user_id}`), {
@@ -63,6 +92,14 @@ export default class UserManager {
       return new User(responseData);
    }
 
+   /**
+    * Delete a user by their ID.
+    *
+    * @param {string} userId - The ID of the user to be deleted.
+    * @returns {Promise<string>} A Promise that resolves to a string message confirming deletion.
+    * @throws {NotFoundError} If the request no user is found for the given ID.
+    * @throws {APIError} If the request fails.
+    */
    async delete(userId: string): Promise<string> {
       const response = await handleRequest(
          fetch(this.getFullUrl(`/user/${userId}`), {
@@ -75,6 +112,14 @@ export default class UserManager {
       return response.text();
    }
 
+   /**
+    * List all users.
+    *
+    * @param {number} limit - The maximum number of users to return.
+    * @param {number} cursor - The index to start listing users from.
+    * @returns {Promise<User[]>} A Promise that resolves to an array of User objects.
+    * @throws {APIError} If the request fails.
+    */
    async list(limit?: number, cursor?: number): Promise<User[]> {
       const params = new URLSearchParams();
       if (limit !== undefined) params.append("limit", limit.toString());
@@ -91,6 +136,14 @@ export default class UserManager {
       return data.map((user: any) => new User(user));
    }
 
+   /**
+    * Get all sessions for a user.
+    *
+    * @param {string} userId - The ID of the user to retrieve sessions for.
+    * @returns {Promise<Session[]>} A Promise that resolves to an array of Session objects.
+    * @throws {NotFoundError} If no sessions are found for the given user ID.
+    * @throws {APIError} If the request fails.
+    */
    async getSessions(userId: string): Promise<Session[]> {
       const response = await handleRequest(
          fetch(this.getFullUrl(`/user/${userId}/sessions`), {
@@ -104,6 +157,16 @@ export default class UserManager {
       return data.map((session: any) => new Session(session));
    }
 
+   /**
+    * List users in chunks.
+    *
+    * This method retrieves users in chunks of a specified size.
+    * It is a generator function that yields each chunk of users as they are retrieved.
+    *
+    * @param {number} chunkSize - The size of the user chunks to retrieve. Defaults to 100.
+    * @yields {Promise<User[]>} A Promise that resolves to an array of User objects.
+    * @throws {APIError} If the request fails.
+    */
    async *listChunked(chunkSize: number = 100) {
       let cursor: number | undefined;
 
