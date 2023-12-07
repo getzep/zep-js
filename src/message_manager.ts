0a4e7715568566b4324c0a8c2dda6ee78d2583ac
Peter <pb@chaos.local>
2023-12-07 16:13:44 -0700

    Message models and manager

---
+++ src/message_manager.ts
@@ -0,0 +1,101 @@
+import { Message } from "./message_models";
+
+import { IZepClient } from "./interfaces";
+import { handleRequest } from "./utils";
+
+export default class MessageManager {
+   client: IZepClient;
+
+   constructor(client: IZepClient) {
+      this.client = client;
+   }
+
+   async getSessionMessages(
+      sessionId: string,
+      limit?: number = 100,
+      offset?: number = 1,
+   ): Promise<Message[]> {
+      if (!sessionId || sessionId.trim() === "") {
+         throw new Error("sessionId must be provided");
+      }
+
+      const response = await handleRequest(
+         fetch(
+            this.client.getFullUrl(
+               `/sessions/${sessionId}/messages?pageSize=${limit}&pageNumber=${offset}`,
+            ),
+            {
+               headers: this.client.headers,
+            },
+         ),
+         `No session found for session ${sessionId}`,
+      );
+
+      const responseData = await response.json();
+
+      return responseData.map((message: any) => new Message(message));
+   }
+
+   async getSessionMessage(
+      sessionId: string,
+      messageId: string,
+   ): Promise<Message> {
+      if (!sessionId || sessionId.trim() === "") {
+         throw new Error("sessionId must be provided");
+      }
+
+      if (!messageId || messageId.trim() === "") {
+         throw new Error("messageId must be provided");
+      }
+
+      const response = await handleRequest(
+         fetch(
+            this.client.getFullUrl(
+               `/sessions/${sessionId}/messages/${messageId}`,
+            ),
+            {
+               headers: this.client.headers,
+            },
+         ),
+         `No session found for session ${sessionId}`,
+      );
+
+      const responseData = await response.json();
+
+      return new Message(responseData);
+   }
+
+   async updateSessionMessageMetadata(
+      sessionId: string,
+      messageId: string,
+      metadata: Record<string, any>,
+   ): Promise<Message> {
+      if (!sessionId || sessionId.trim() === "") {
+         throw new Error("sessionId must be provided");
+      }
+
+      if (!messageId || messageId.trim() === "") {
+         throw new Error("messageId must be provided");
+      }
+
+      const response = await handleRequest(
+         fetch(
+            this.client.getFullUrl(
+               `/sessions/${sessionId}/messages/${messageId}`,
+            ),
+            {
+               method: "PATCH",
+               headers: this.client.headers,
+               body: JSON.stringify({
+                  metadata,
+               }),
+            },
+         ),
+         `No session found for session ${sessionId}`,
+      );
+
+      const responseData = await response.json();
+
+      return new Message(responseData);
+   }
+}
