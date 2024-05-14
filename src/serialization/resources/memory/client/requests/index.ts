de42b893124246a29a71f1be8434d4a49482695d
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-05-14 04:42:20 +0000

    SDK regeneration

--- src/serialization/resources/memory/client/requests/index.ts
+++ src/serialization/resources/memory/client/requests/index.ts
@@ -1,6 +1,7 @@
 export { CreateSessionRequest } from "./CreateSessionRequest";
 export { UpdateSessionRequest } from "./UpdateSessionRequest";
 export { ClassifySessionRequest } from "./ClassifySessionRequest";
+export { EndSessionRequest } from "./EndSessionRequest";
 export { ModelsExtractDataRequest } from "./ModelsExtractDataRequest";
 export { AddMemoryRequest } from "./AddMemoryRequest";
 export { ModelsMessageMetadataUpdate } from "./ModelsMessageMetadataUpdate";
