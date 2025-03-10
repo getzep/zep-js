c17c4a9f66e6c11f869259aaa2d963b6ca73d141
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-03-10 21:45:28 +0000

    SDK regeneration

--- src/serialization/resources/memory/client/requests/AddMemoryRequest.ts
+++ src/serialization/resources/memory/client/requests/AddMemoryRequest.ts
@@ -5,11 +5,13 @@
 import * as serializers from "../../../../index";
 import * as Zep from "../../../../../api/index";
 import * as core from "../../../../../core";
+import { RoleType } from "../../../../types/RoleType";
 import { Message } from "../../../../types/Message";
 
 export const AddMemoryRequest: core.serialization.Schema<serializers.AddMemoryRequest.Raw, Zep.AddMemoryRequest> =
     core.serialization.object({
         factInstruction: core.serialization.property("fact_instruction", core.serialization.string().optional()),
+        ignoreRoles: core.serialization.property("ignore_roles", core.serialization.list(RoleType).optional()),
         messages: core.serialization.list(Message),
         returnContext: core.serialization.property("return_context", core.serialization.boolean().optional()),
         summaryInstruction: core.serialization.property("summary_instruction", core.serialization.string().optional()),
@@ -18,6 +20,7 @@ export const AddMemoryRequest: core.serialization.Schema<serializers.AddMemoryRe
 export declare namespace AddMemoryRequest {
     export interface Raw {
         fact_instruction?: string | null;
+        ignore_roles?: RoleType.Raw[] | null;
         messages: Message.Raw[];
         return_context?: boolean | null;
         summary_instruction?: string | null;
