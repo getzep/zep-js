43fe99354ffec2c40ed46495435761fb90878b9a
paulpaliychuk <pavlo.paliychuk.ca@gmail.com>
2025-07-30 15:15:16 -0400

    chore: remove memory wrapper

--- src/wrapper/index.ts
+++ src/wrapper/index.ts
@@ -1,12 +1,7 @@
 import { ZepClient as BaseClient } from "../Client";
-import { Memory } from "./memory";
 import { Graph } from "./graph";
 
 export class ZepClient extends BaseClient {
-    public get memory(): Memory {
-        return new Memory(this._options);
-    }
-
     public get graph(): Graph {
         return new Graph(this._options);
     }
