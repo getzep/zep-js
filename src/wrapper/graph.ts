c3242a22624f68ce4e2b5c64e29dcdbfdcbcbf17
paulpaliychuk <pavlo.paliychuk.ca@gmail.com>
2025-08-05 18:06:50 -0400

    chore: Add support for user and graph specific entity/edge types

--- src/wrapper/graph.ts
+++ src/wrapper/graph.ts
@@ -62,6 +62,8 @@ export class Graph extends BaseGraph {
             {
                 entityTypes: validatedEntityTypes,
                 edgeTypes: validatedEdgeTypes,
+                userId,
+                graphId,
             },
             requestOptions,
         );
